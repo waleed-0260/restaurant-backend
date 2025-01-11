d0dc295d6483c2b058a0107866cf4d3708376ea8
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2025-01-10 22:36:18 -0800

    add

--- models/portfolio.js
+++ models/portfolio.js
@@ -1,17 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const portfolioSchema = new mongoose.Schema({
-    heading:{type:String, required: true},
-    tagline:{type:String, required: true},
-    tags: [{type:String, required: true}],
-    description:{type: String, required: true},
-    descriptionImage:{type:String, required: true},
-    challenge:{type:String, requried: true},
-    challengeImage: {type:String, required:true},
-    solution:{type:String, requried: true},
-    solutionImage: {type:String, required:true},
-}, { timestamps: true } );
+const portfolioSchema = new mongoose.Schema(
+  {
+    heading: { type: String, required: true },
+    tagline: { type: String, required: true },
+    tags: [{ type: String, required: true }],
+    description: { type: String, required: true },
+    masterFloorImage: { type: String, required: true },
+    panelFloorImage: { type: String, required: true },
+    challenge: { type: String, required: true },
+    mapImage: { type: String, required: true },
+    renderingImage: { type: String, required: true },
+    additionalImage: { type: String, required: true }, // Added the "one more image" field
+    solution: { type: String, required: true }, // Keeping solution unchanged
+    solutionImage: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const portfolio = mongoose.model("portfolioSchema", portfolioSchema)
+const Portfolio = mongoose.model("Portfolio", portfolioSchema);
 
-module.exports = portfolio;
+module.exports = Portfolio;
